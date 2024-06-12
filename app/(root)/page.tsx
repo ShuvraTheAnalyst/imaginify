e66ce99fa53e4b133114497d633feeb5c6d5491a
Shuvra Roy <shuvraroy88@gmail.com>
2024-06-12 13:01:23 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { UserButton } from '@clerk/nextjs'
+
+
+const Home = () => {
+  return (
+    <div>
+      <p></p>
+
+
+      {/* <UserButton afterSignOutUrl='/'  /> */}
+    </div>
+  )
+}
+
+export default Home
